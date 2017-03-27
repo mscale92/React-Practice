288c44cf02785b31260187732a5b886c4affde0a
mscale92 <scalemary@gmail.com>
2017-03-26 19:53:35 -0700

    added a server.js file, using express, which runs on a .env port, if no port is provided, localhost:1337, added some key files to the src folder, and altered the webpack.config.js file so that it is working, phew

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,40 @@
-const webpack = require('webpack');
-
 module.exports = {
-    entry: './src/js/app.js',
-    output: {
-        filename: './src/js/app-bundle.js'
-    },
-    resolve: {
-        extensions: ['', '.js', '.jsx']
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?/,
-                loader: 'babel',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    devtool: 'source-map',
-}
+  context: __dirname + "/src",
 
-/*new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    })*/
+  entry: "./js/app.js",
+
+  output: {
+    filename: "./src/js/app-bundle.js",
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.json']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ["babel-loader"]
+      }
+    ]
+  }
+};
+
+/*What is going on above?
+    context - tells webpack where our files will be located
+        __dirname - our current directory
+        (in this case, + "/src" because our js files reside in the src folder)
+
+    entry - the main application file, the start of the pipeline
+
+    output - where our files will be outputted
+    (in this case, we will output our js into another file in the same directory aptly called app-bundle.js)
+
+    resolve - TO DO
+
+    loaders - functions that transform files
+        the babel-loaders test for .jsx files and exclude any node_module files
+
+
+
+*/
